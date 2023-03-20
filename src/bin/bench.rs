27f2018667e6e41aceb41095e5493f4b4f987e60
Manvir Singh <mail@manvir.me>
2023-03-20 14:38:57 +0530

    Separate bin from lib

---
+++ src/bin/bench.rs
@@ -0,0 +1,49 @@
+use std::collections::{HashMap, HashSet};
+use tuid::Tuid;
+
+const SIZE: usize = 1_000_000;
+
+fn main() {
+    let mut data = Vec::with_capacity(SIZE);
+
+    let mut counter = 0;
+    loop {
+        counter += 1;
+        if counter > SIZE {
+            break;
+        }
+
+        let id = Tuid::new().unwrap();
+        data.push(id.to_string());
+    }
+
+    let mut map: HashMap<String, u16> = HashMap::with_capacity(SIZE);
+    for id in data {
+        match map.get_mut(&id) {
+            None => {
+                map.insert(id, 1);
+            }
+            Some(val) => {
+                *val += 1;
+            }
+        }
+        // let counter = map.entry(&id).or_insert(0);
+        // *counter += 1;
+    }
+
+    let mut counter = 0;
+
+    for (key, value) in map.into_iter() {
+        if value > 1 {
+            counter += 1;
+            println!("{key} -> {value}");
+        }
+    }
+
+    println!(
+        "total {}, unique {}, duplicates {}",
+        SIZE,
+        SIZE - counter,
+        counter
+    );
+}
